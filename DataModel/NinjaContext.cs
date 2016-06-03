57dd4c1daae8879b53a7afe212e3143c08194e2d
stanleynik <stanleynik92@gmail.com>
2016-06-02 18:13:47 -0600

    EF6Practices NinjaEx Initial

---
+++ DataModel/NinjaContext.cs
@@ -0,0 +1,21 @@
+﻿using NinjaDomain.Classes;
+using System.Data.Entity;
+
+namespace NinjaDomain.DataModel
+{
+    public class NinjaContext: DbContext
+    {
+
+        public NinjaContext()
+            : base("name=DefaultConnectionString")
+        {
+        }
+
+        public DbSet<Ninja> Ninjas { get; set; }
+
+        public DbSet<Clan> Clans { get; set; }
+
+        public DbSet<NinjaEquipment> Equipment { get; set; }
+    }
+
+}
